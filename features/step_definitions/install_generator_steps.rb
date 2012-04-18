fce45da8f2e5ee0032030c14020d19b6ba1b84c1
Ben Langfeld <ben@langfeld.me>
2012-04-18 12:01:21 +0200

    [BUGFIX] Use the correct gem name in features

--- features/step_definitions/install_generator_steps.rb
+++ features/step_definitions/install_generator_steps.rb
@@ -1,6 +1,6 @@
 When /^this gem is installed in that application$/ do
   gempath = File.expand_path('../../../', __FILE__)
-  step "I append to \"Gemfile\" with \"gem 'plugin-demo', :path => '#{gempath}'\""
+  step "I append to \"Gemfile\" with \"gem 'voicemail', :path => '#{gempath}'\"\n"
   step "I successfully run `bundle install`"
 end
 
