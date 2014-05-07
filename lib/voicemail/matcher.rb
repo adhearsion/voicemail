9ff2a8508a692c2b81b5a50c6376c2700eb46ca9
Ben Klang <bklang@mojolingo.com>
2014-05-07 12:51:18 -0400

    Update: Yes, this class is necessary

--- lib/voicemail/matcher.rb
+++ lib/voicemail/matcher.rb
@@ -1,3 +1,6 @@
+# encoding: utf-8
+# This template exists for the purpose of being overridden.  See the README for more information.
+
 module Voicemail
   class Matcher
     def initialize(entered, actual)
