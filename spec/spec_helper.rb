1a9a42c44531b7951b6ccc98cffd86635dbded32
Ben Langfeld <ben@langfeld.me>
2015-01-23 12:55:17 -0200

    Fix RSpec deprecation warning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'voicemail'
 Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
-  config.color_enabled = true
+  config.color = true
   config.tty = true
 
   config.mock_framework = :flexmock
